439517d46fe75296882492ac29a3f8b6b4db125c
Augustin Mesquita <augustomesquitasrs@gmail.com>
2018-05-31 13:39:51 -0300

    array.js atualizado.

--- arrays.js
+++ arrays.js
@@ -1,13 +1,34 @@
-var myArrayOfObjects = [{"id":"14","label":"7"},{"id":"15","label":"7"},{"id":"18","label":"7"}];
-var indexFound = myArrayOfObjects.findIndex(myObject => myObject.id == "14");
+// Cria array de objetos.
+var myArrayOfObjects = [
+  { id: "18", label: "7" },
+  { id: "14", label: "3" },
+  { id: "16", label: "2" }
+];
 
-console.log("----------------------------------------------------")
-console.log("Index do item de id '14' que será removido: " + indexFound);
+// Ordena array com objeos de menor indice para o maior.
+myArrayOfObjects.sort((a, b) => {
+  return a.id > b.id; // Em caso positivo, o elemento 'b' assume posição do elemento 'a'.
+});
 
-if (indexFound || indexFound == 0) {
-    console.log("----------------------------------------------------")
-    myArrayOfObjects.forEach(object => console.log("ID de itens da lista antes de remover: " + object.id));    
-    myArrayOfObjects.splice(indexFound, 1); // Remove apenas 1 item da lista a partir do indice inidicado.
-    console.log("----------------------------------------------------")
-    myArrayOfObjects.forEach(object => console.log("ID de itens da lista depois de remover: " + object.id));
+// Procura indice do elemento de id '14'.
+var idToSearch = "1";
+var indexFound = myArrayOfObjects.findIndex(myObject => myObject.id === idToSearch);
+
+console.log("----------------------------------------------------");
+console.log("Index do item de id " + idToSearch + " que será removido: " + indexFound);
+console.log("----------------------------------------------------");
+
+if (indexFound > -1) {
+  myArrayOfObjects.forEach(object =>
+    console.log("ID de itens da lista antes de remover: " + object.id)
+  );
+
+  myArrayOfObjects.splice(indexFound, 1); // Remove apenas 1 item da lista a partir do indice inidicado.
+  console.log("----------------------------------------------------");
+  
+  myArrayOfObjects.forEach(object =>
+    console.log("ID de itens da lista depois de remover: " + object.id)
+  );
+} else {
+    console.log("Falha ao remover item. ID não encontrado.")
 }
